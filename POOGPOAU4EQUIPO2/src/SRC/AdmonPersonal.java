b29e484fa1df6b6db0512892a6780503d40dafd9
JanPol2000 <46852049+JanPol2000@users.noreply.github.com>
2019-05-06 17:16:30 -0500

    Constructor Admon
    Asi nomas quedo :v

--- POOGPOAU4EQUIPO2/src/SRC/AdmonPersonal.java
+++ POOGPOAU4EQUIPO2/src/SRC/AdmonPersonal.java
@@ -2,7 +2,16 @@
 package SRC;
 
 public class AdmonPersonal {
-    Personal personal[] = new Personal[99];
+    private Personal personal[];
     private int cont;
+    
+    public AdmonPersonal(){
+        personal  = new Personal[99];
+        cont = 0;
+    }
+    
+    public int getCont(){
+        return cont;
+    }
     
 }
